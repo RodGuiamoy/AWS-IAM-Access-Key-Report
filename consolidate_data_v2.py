d0a841e1c6c09e1a8d49434dc026cf9a3a2c0982
RodGuiamoy <janrudolfguiamoy@gmail.com>
2024-06-26 09:31:27 -0400

    fixed filter in consolidate data py

--- consolidate_data_v2.py
+++ consolidate_data_v2.py
@@ -8,7 +8,7 @@ files = os.listdir()
 
 # Filter out the .csv files
 # csv_files = [file for file in files if file.endswith('.csv')]
-csv_files = [file for file in files if file.endswith('.csv') and not file.startswith('AWS Console Access Report')]
+csv_files = [file for file in files if file.endswith('.csv') and not file.startswith('AWS Access Key Report')]
 
 csv_files.sort()  # Sort the list in place
 
